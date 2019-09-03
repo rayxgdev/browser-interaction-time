f867d26dde444245751971ce450ddb0fdb30d60a
rayxgdev <manuelrodriguezgil@informaticos.com>
2019-09-03 08:19:30 +0200

    Change localKey at demo/index.js to regular name

--- dist/browser-interaction-time.es5.js
+++ dist/browser-interaction-time.es5.js
@@ -527,7 +527,6 @@ var BrowserInteractionTime = /** @class */ (function () {
     };
     return BrowserInteractionTime;
 }());
-//# sourceMappingURL=browser-interaction-time.js.map
 
 export default BrowserInteractionTime;
 //# sourceMappingURL=browser-interaction-time.es5.js.map
